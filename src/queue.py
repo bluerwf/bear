1989c7946e27448b14c7c1968120c59764b0eb61
bluerwf <bluerwf@gmail.com>
2015-07-07 22:01:23 +0800

    add stack

--- src/queue.py
+++ src/queue.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 # coding=utf-8
-
 class Queue:
     def __init__(self, size):
 
@@ -28,3 +27,50 @@ class Queue:
     @property
     def length(self):
         return len(self)
+
+class AbsStack:
+    def push(self, i):
+        raise NotImplemented
+
+    def pop(self):
+        raise NotImplemented
+
+class Stack(AbsStack):
+    def __init__(self, size):
+        self._lst = []
+        self.size = size
+    
+    def push(self, i):
+        if len(self._lst) < self.size:
+            self._lst.append(i)
+        else:
+            print'full'
+
+    def pop(self):
+        try:
+            v = self._lst.pop()
+            return v
+        except IndexError:
+            print 'this stack is empty'
+
+    @property
+    def top(self):
+        try:
+            return self._lst[-1]
+        except IndexError:
+            return None
+
+    @property
+    def base(self):
+        try:
+            return self._lst[0]
+        except IndexError:
+            return None
+
+    def __str__(self):
+        return str(self._lst)
+
+sstack = Stack(10)
+
+sstack.push(1)
+sstack.push(2)
